fc5600b8be33d0654ede29301fac4c3a580e3430
Javad <J.Evazzadeh@live.com>
2016-06-17 20:00:02 +0430

    update database files

--- includes/cls/database/install/(db_name)+/05_cardusages.sql
+++ includes/cls/database/install/(db_name)+/05_cardusages.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2016-06-16 19:41:13
+Date: 2016-06-17 19:59:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,19 +20,20 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `cardusages`;
 CREATE TABLE `cardusages` (
-  `cardlist_id` int(11) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
+  `cardlist_id` int(11) unsigned NOT NULL,
+  `cardusage_answer` enum('true','false','skip','') NOT NULL,
   `cardusage_deck` smallint(5) unsigned DEFAULT NULL,
   `cardusage_try` smallint(5) unsigned DEFAULT NULL,
   `cardusage_trysuccess` smallint(5) unsigned DEFAULT NULL,
+  `cardusage_spendtime` smallint(5) unsigned DEFAULT NULL,
   `cardusage_expire` datetime DEFAULT NULL,
   `cardusage_lasttry` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `cardusage_status` enum('enable','disable','expire') NOT NULL DEFAULT 'enable',
+  PRIMARY KEY (`id`),
   KEY `cardusages_users_id` (`user_id`),
   KEY `cardusages_ibfk_4` (`cardlist_id`),
   CONSTRAINT `cardusages_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `cardusages_ibfk_4` FOREIGN KEY (`cardlist_id`) REFERENCES `cardlists` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of cardusages
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
