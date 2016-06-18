e241323826997299b741ae0059aea92ea142dc1b
Javad <J.Evazzadeh@live.com>
2016-06-19 03:05:05 +0430

    update azvir site desc

--- includes/mvc/view.php
+++ includes/mvc/view.php
@@ -8,10 +8,11 @@ class view extends \lib\mvc\view
 		// define default value for global
 
 		$this->data->site['title']   = T_("Azvir");
-		$this->data->site['desc']    = T_("Ermile is new");
+		$this->data->site['desc']    = T_("Azvir"). ' '. T_("uses Leitner flashcards to make memorizing facts"). ' '. T_("not only more efficient but also more fun.");
 		$this->data->site['slogan']  = T_("Ermile is our company");
 
 		$this->data->page['desc']    = T_("Ermile is Inteligent.");
+		$this->data->page['desc']    = $this->data->site['desc'];
 
 
 		// $this->url->MainStatic       = false;
