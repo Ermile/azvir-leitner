8fa9593fe3b2ac64e6d6faee6d57bd75c243fd4e
Javad <J.Evazzadeh@live.com>
2016-06-29 03:20:02 +0430

    update trans

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -6,27 +6,31 @@ private function transtext()
 	echo T_("Home");                                                                  // Line 22
 	echo T_("About");                                                                 // Line 23
 	echo T_("Features");                                                              // Line 24
-	echo T_("Learn & Enjoy");                                                         // Line 35
-	echo T_("uses Leitner flashcards to make memorizing facts");                      // Line 36
-	echo T_("not only more efficient but also more fun.");                            // Line 36
-	echo T_("Start learn with fun");                                                  // Line 38
-	echo T_("ABOUT US");                                                              // Line 47
-	echo T_("Who we are");                                                            // Line 51
-	echo T_("We are some developer and try to create usefull tools. our another tools is comming soon");// Line 52
-	echo T_("Why choose our service?");                                               // Line 53
-	echo T_("In past we use <a href='http://sourceforge.net/projects/jmemorize/' target='_blank'>jmemorize</a> and enjoy using it.");// Line 54
-	echo T_("It written in Java and depending on Os.");                               // Line 54
-	echo T_("We decide to create a service like jmemorize and help people to learn with Leitner flashcard with fun and enjoy using it.");// Line 54
-	echo T_("OUR FEATURES");                                                          // Line 67
-	echo T_("manages your learn progress and features categories, Unicode flashcard texts, statistics and an intuitive interface.");// Line 70
-	echo T_("Responsive Design");                                                     // Line 73
-	echo T_("You can use our service in all of your device in any resolution.");      // Line 74
-	echo T_("Web Development");                                                       // Line 77
-	echo T_("We develope features and improve usability of this service.");           // Line 78
-	echo T_("Learn Faster");                                                          // Line 81
-	echo T_("with this service based on Leitner flashcard we help you learn faster than usual and enjoy!");// Line 82
-	echo T_("Copyright");                                                             // Line 94
-	echo T_("2015");                                                                  // Line 94
+	echo T_("Learn & Enjoy");                                                         // Line 38
+	echo T_("uses Leitner flashcards to make memorizing facts");                      // Line 39
+	echo T_("not only more efficient but also more fun.");                            // Line 39
+	echo T_("Start learn with fun");                                                  // Line 41
+	echo T_("ABOUT US");                                                              // Line 50
+	echo T_("Who we are");                                                            // Line 54
+	echo T_("We are some developer and try to create usefull tools. our another tools is comming soon");// Line 55
+	echo T_("Why choose our service?");                                               // Line 56
+	echo T_("In past we use <a href='http://sourceforge.net/projects/jmemorize/' target='_blank'>jmemorize</a> and enjoy using it.");// Line 57
+	echo T_("It written in Java and depending on Os.");                               // Line 57
+	echo T_("We decide to create a service like jmemorize and help people to learn with Leitner flashcard with fun and enjoy using it.");// Line 57
+	echo T_("OUR FEATURES");                                                          // Line 70
+	echo T_("manages your learn progress and features categories, Unicode flashcard texts, statistics and an intuitive interface.");// Line 73
+	echo T_("Responsive Design");                                                     // Line 76
+	echo T_("You can use our service in all of your device in any resolution.");      // Line 77
+	echo T_("Web Development");                                                       // Line 80
+	echo T_("We develope features and improve usability of this service.");           // Line 81
+	echo T_("Learn Faster");                                                          // Line 84
+	echo T_("with this service based on Leitner flashcard we help you learn faster than usual and enjoy!");// Line 85
+	echo T_("Copyright");                                                             // Line 97
+	echo T_("2015");                                                                  // Line 97
+	echo T_("Powered by");                                                            // Line 97
+	echo T_("Saloos");                                                                // Line 97
+	echo T_("in");                                                                    // Line 97
+	echo T_("Ermile");                                                                // Line 97
 
 	//--------------------------------------------------content_app\cards\display.html
 	echo T_("Actions");                                                               // Line 18
