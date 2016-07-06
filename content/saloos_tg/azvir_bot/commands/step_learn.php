09e3345b328fca0e922b32b2291b7df300e33a21
Javad <J.Evazzadeh@live.com>
2016-07-06 18:06:24 +0430

    check on server

--- content/saloos_tg/azvir_bot/commands/step_learn.php
+++ content/saloos_tg/azvir_bot/commands/step_learn.php
@@ -733,6 +733,7 @@ class step_learn
 
 		// get max row to draw only until top row
 		$maxRow = max($datalist);
+		var_dump($maxRow);
 
 		// draw 4 deck in chart
 		for ($i=1; $i < 4; $i++)
@@ -759,7 +760,7 @@ class step_learn
 						$chart_row .= $deck;
 					}
 				}
-				if($i > $maxRow)
+				elseif($i > $maxRow)
 				{
 					// do nothing
 				}
